87c573378f96b197655fe4de647e9a556b95cd20
Projeto <projeto@iMac-de-Senac-4.local>
2018-06-18 17:42:02 -0300

    Segundo commit

---
+++ app/src/main/java/com/example/projeto/applistacompras/Controller/Item.java
@@ -0,0 +1,47 @@
+package com.example.projeto.applistacompras.Controller;
+
+public class Item {
+    private int id;
+    private double quantidade;
+    private String nome;
+    private boolean check;
+
+
+
+
+
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(double quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public boolean isCheck() {
+        return check;
+    }
+
+    public void setCheck(boolean check) {
+        this.check = check;
+    }
+}
---
+++ app/src/main/java/com/example/projeto/applistacompras/Controller/Lista.java
@@ -0,0 +1,37 @@
+package com.example.projeto.applistacompras.Controller;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Lista {
+
+    private int id;
+    private Date data;
+    private ArrayList<Item> listaDeItens;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public ArrayList<Item> getListaDeItens() {
+        return listaDeItens;
+    }
+
+    public void setListaDeItens(ArrayList<Item> listaDeItens) {
+        this.listaDeItens = listaDeItens;
+    }
+}
