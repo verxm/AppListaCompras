09a16fb5726fda5ed6675c579e6ba9ea04d9636a
FeiraProjetos2018 <mzeroinfo@gmail.com>
2018-06-19 16:10:37 -0300

    Correcao

--- app/src/main/java/com/example/projeto/applistacompras/Controller/ItemDAO.java
+++ app/src/main/java/com/example/projeto/applistacompras/Controller/ItemDAO.java
@@ -11,34 +11,9 @@ import com.example.projeto.applistacompras.View.NovaListaActivity;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ItensCompraDAO {
-
-    public static void excluirDados(final int id, Item p, final Context contexto) {
-
-        String texto = "Nome: " + p.getNome() + "\n" +
-                "Quantidade: " + p.getQuantidade() + "\n" +
-                "\n\nDeseja excluir o item?";
-
-        AlertDialog.Builder alerta = new AlertDialog.Builder(contexto);
-        alerta.setTitle("Excluir Item");
-        alerta.setIcon(android.R.drawable.ic_dialog_alert);
-        alerta.setMessage(texto);
+public class ItemDAO {
 
 
-        alerta.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                excluir(id, contexto);
-                NovaListaActivity nl = (NovaListaActivity) contexto;
-                nl.carregarItens();
-
-            }
-
-
-        });
-        alerta.setNegativeButton("Não", null);
-        alerta.show();
-    }
 
     public static void editar(Item p, Context contexto) {
         String sql = "UPDATE item SET " +
---
+++ app/src/main/java/com/example/projeto/applistacompras/Controller/ListaDAO.java
@@ -0,0 +1,9 @@
+package com.example.projeto.applistacompras.Controller;
+
+public class ListaDAO {
+
+
+
+
+
+}
