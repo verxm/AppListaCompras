0bc2af8a23dd6586971ae3a4f5bdfa5bb8d8900e
FeiraProjetos2018 <mzeroinfo@gmail.com>
2018-06-19 19:09:27 -0300

    Correcao

--- app/src/main/java/com/example/projeto/applistacompras/View/ListasActivity.java
+++ app/src/main/java/com/example/projeto/applistacompras/View/ListasActivity.java
@@ -3,7 +3,7 @@ package com.example.projeto.applistacompras.View;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.FloatingActionButton; 
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
